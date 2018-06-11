99e013b754a56591ebb34dab57bc577a3cdecaf1
rossmcarthur <r.mcarth20@gmail.com>
2018-06-11 13:45:07 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <p1>Hello from the homepage!</p1>
+      </div>
+    )
+  }
+}
+
+export default Homepage;
