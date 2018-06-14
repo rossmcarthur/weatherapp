63ef089edda14b2d1aabfa613df103e310c35718
rossmcarthur <r.mcarth20@gmail.com>
2018-06-14 06:51:20 -0400

    Updated homepage stylings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import Homepage from './home';
 import "../styles/App.css";
+import logo from '../styles/images/logo.png';
+import Weatherboard from './weatherboard';
+import Modal from './modal';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: null
+    };
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  showModal(city) {
+    this.setState({ city });
+  }
+
+  closeModal() {
+    this.setState({ city: null });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Homepage />
+      <div className='app-page'>
+        <div className='homepage-header'>
+          <img className='logo' src={logo} alt='logo'/>
+          <h1 className='title'>Weather Vane</h1>
+        </div>
+        <div className='homepage'>
+          <Modal city={this.state.city} closeModal={this.closeModal}/>
+            <Weatherboard showModal={this.showModal}/>
+        </div>
       </div>
     );
   }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import Weatherboard from './weatherboard';
-import Modal from './modal';
-
-class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: null
-    };
-    this.showModal = this.showModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-
-  showModal(city) {
-    this.setState({ city });
-  }
-
-  closeModal() {
-    this.setState({ city: null });
-  }
-
-  render() {
-    return (
-      <div className='homepage'>
-        <Modal city={this.state.city} closeModal={this.closeModal}/>
-        <Weatherboard showModal={this.showModal}/>
-      </div>
-    );
-  }
-}
-
-export default Homepage;
+// import React from 'react';
+// import Weatherboard from './weatherboard';
+// import Modal from './modal';
+//
+// class Homepage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       city: null
+//     };
+//     this.showModal = this.showModal.bind(this);
+//     this.closeModal = this.closeModal.bind(this);
+//   }
+//
+//   showModal(city) {
+//     this.setState({ city });
+//   }
+//
+//   closeModal() {
+//     this.setState({ city: null });
+//   }
+//
+//   render() {
+//     return (
+//       <div className='homepage'>
+//         <Modal city={this.state.city} closeModal={this.closeModal}/>
+//         <Weatherboard showModal={this.showModal}/>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default Homepage;
