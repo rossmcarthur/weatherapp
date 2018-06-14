f2fa9932bbd3b4b47c3756bebda836470b65e076
rossmcarthur <r.mcarth20@gmail.com>
2018-06-14 07:05:05 -0400

    Fix api accessibility

--- src/ajax/api.js
+++ src/ajax/api.js
@@ -1,19 +1,19 @@
-import * as Keys from '../keys';
+const APIKey = "6a190bd8ee47493f099115e05b143028";
 
 export const fetchWeather = (id, units) => {
-  return fetch(`http://api.openweathermap.org/data/2.5/group?id=${id}&units=${units}&APPID=${Keys.APIKey}`, {
+  return fetch(`http://api.openweathermap.org/data/2.5/group?id=${id}&units=${units}&APPID=${APIKey}`, {
     method: 'get'
   });
 };
 
 export const fetchForecast = (id, units) => {
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${id}&units=${units}&APPID=${Keys.APIKey}`, {
+  return fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${id}&units=${units}&APPID=${APIKey}`, {
     method: 'get'
   });
 };
 
 export const fetchCurrent = (id, units) => {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?id=${id}&units=${units}&APPID=${Keys.APIKey}`, {
+  return fetch(`http://api.openweathermap.org/data/2.5/weather?id=${id}&units=${units}&APPID=${APIKey}`, {
     method: 'get'
   });
 };
